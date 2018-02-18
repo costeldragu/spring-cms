da57179f8d93b17c585727b97ca3761a29d99d0a
Kostas Kokkoros <mavroprovato@gmail.com>
2018-02-18 02:13:14 +0200

    Initial project skeleton

---
+++ src/main/java/net/mavroprovato/springcms/SpringCmsApplication.java
@@ -0,0 +1,12 @@
+package net.mavroprovato.springcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCmsApplication.class, args);
+    }
+}
