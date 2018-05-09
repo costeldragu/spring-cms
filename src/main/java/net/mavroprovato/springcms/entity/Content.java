ff15d0f701459e39d8f592169a9f5e9a925f3525
Kostas Kokkoros <mavroprovato@gmail.com>
2018-05-09 23:03:47 +0300

    Add documentation.

--- src/main/java/net/mavroprovato/springcms/entity/Content.java
+++ src/main/java/net/mavroprovato/springcms/entity/Content.java
@@ -7,72 +7,118 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+/**
+ * Holds the content.
+ */
 @Entity
 public class Content {
 
+    /** The unique identifier of the content */
     @Id
     @SequenceGenerator(name = "content_generator", sequenceName = "content_sequence", allocationSize = 1)
     @GeneratedValue(generator = "content_generator")
     private Integer id;
 
+    /** The content title */
     @Column(nullable = false)
     private String title;
 
+    /** The content */
     @Column(nullable = false)
     @Type(type = "org.hibernate.type.TextType")
     private String content;
 
+    /** The content creation date */
     @Column(nullable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    /** The content update date */
     @Column(nullable = false)
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    /** The content publication date */
     @Column
     private LocalDateTime publishedAt;
 
+    /**
+     * Return the content identifier.
+     *
+     * @return The content identifier.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Return the content title.
+     *
+     * @return The content title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the content title.
+     *
+     * @param title The content title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Return the content.
+     *
+     * @return The content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Set the content.
+     *
+     * @param content The content.
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Return the content creation date.
+     *
+     * @return The content creating date.
+     */
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
+    /**
+     * Return the content update date.
+     *
+     * @return The content creating date.
+     */
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
+    /**
+     * Return the content publication date.
+     *
+     * @return The content publication date.
+     */
     public LocalDateTime getPublishedAt() {
         return publishedAt;
     }
 
+    /**
+     * Set the content publication date.
+     *
+     * @param publishedAt The publication date.
+     */
     public void setPublishedAt(LocalDateTime publishedAt) {
         this.publishedAt = publishedAt;
     }
