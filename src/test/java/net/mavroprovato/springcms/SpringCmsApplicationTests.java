938c44a4c1eec384a4f220c32c62137cc8b89cc0
Kostas Kokkoros <mavroprovato@gmail.com>
2018-05-25 00:35:58 +0300

    Transform to spaces

--- src/test/java/net/mavroprovato/springcms/SpringCmsApplicationTests.java
+++ src/test/java/net/mavroprovato/springcms/SpringCmsApplicationTests.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCmsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
