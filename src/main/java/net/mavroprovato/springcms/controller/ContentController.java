001424629f3ea52bd688b886c3e778c4ef5c529b
Kostas Kokkoros <mavroprovato@gmail.com>
2018-05-26 01:35:19 +0300

    Return model and view from the controllers

--- src/main/java/net/mavroprovato/springcms/controller/ContentController.java
+++ src/main/java/net/mavroprovato/springcms/controller/ContentController.java
@@ -4,9 +4,9 @@ import net.mavroprovato.springcms.entity.Comment;
 import net.mavroprovato.springcms.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -32,51 +32,42 @@ public class ContentController {
     /**
      * Display the content by its id.
      *
-     * @param model The model.
      * @param id The content id.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/content/{id:\\d+}")
-    public String getById(Model model, @PathVariable("id") int id) {
-        model.addAllAttributes(contentService.getById(id));
-
-        return "content";
+    public ModelAndView getById(@PathVariable("id") int id) {
+        return new ModelAndView("content", contentService.getById(id));
     }
 
     /**
      * Display the content by its slug.
      *
-     * @param model The model.
      * @param slug The content slug.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping("/content/{slug:\\D\\S+}")
-    public String getBySlug(Model model, @PathVariable("slug") String slug) {
-        model.addAllAttributes(contentService.getBySlug(slug));
-
-        return "content";
+    public ModelAndView getBySlug(@PathVariable("slug") String slug) {
+        return new ModelAndView("content", contentService.getBySlug(slug));
     }
 
     /**
      * Post a comment to a content item.
      *
-     * @param model The page model.
      * @param id The post identifier.
      * @param comment The submitted comment.
      * @param bindingResult The form binding result.
-     * @return The template name.
+     * @return The model and view.
      */
     @PostMapping("/content/{id:\\d+}/comment")
-    public String postComment(Model model, @PathVariable("id") int id,
-                              @Valid @ModelAttribute("newComment") Comment comment, BindingResult bindingResult) {
+    public ModelAndView postComment(@PathVariable("id") int id, @Valid @ModelAttribute("newComment") Comment comment,
+                                    BindingResult bindingResult) {
         // Validate the form
         if (bindingResult.hasErrors()) {
-            model.addAllAttributes(contentService.getById(id));
-
-            return "content";
+            return new ModelAndView("content", contentService.getById(id));
         }
         contentService.addComment(id, comment);
 
-        return "redirect:/content/" + id;
+        return new ModelAndView("redirect:/content/" + id);
     }
 }
--- src/main/java/net/mavroprovato/springcms/controller/ContentListController.java
+++ src/main/java/net/mavroprovato/springcms/controller/ContentListController.java
@@ -3,9 +3,9 @@ package net.mavroprovato.springcms.controller;
 import net.mavroprovato.springcms.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * The content list controller.
@@ -29,224 +29,192 @@ public class ContentListController {
     /**
      * Display the first page of content items, ordered by the latest published first.
      *
-     * @param model The page model.
      * @return The page template name.
      */
     @GetMapping(value = "/")
-    public String list(Model model) {
-        return listPage(model, 1);
+    public ModelAndView list() {
+        return listPage(1);
     }
 
     /**
      * Display a page of content items, order by the latest published first.
      *
-     * @param model The page model.
      * @param page The page number.
-     * @return The page template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/page/{page:\\d+}")
-    public String listPage(Model model, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.list(page));
-
-        return "contents";
+    public ModelAndView listPage(@PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.list(page));
     }
 
     /**
      * Display the first page of content items published in a year, ordered by the latest published first.
      *
-     * @param model The model.
      * @param year The year.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}")
-    public String listYear(Model model, @PathVariable("year") int year) {
-        return listYearPage(model, year, 1);
+    public ModelAndView listYear(@PathVariable("year") int year) {
+        return listYearPage(year, 1);
     }
 
     /**
      * Display a page of content items published in a year, ordered by the latest published first.
      *
-     * @param model The page model.
      * @param year The year.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}/page/{page:\\d+}")
-    public String listYearPage(Model model, @PathVariable("year") int year, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.list(year, page));
-
-        return "contents";
+    public ModelAndView listYearPage(@PathVariable("year") int year, @PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.list(year, page));
     }
 
     /**
      * Display the first page of content items published in a month, ordered by the latest published first.
      *
-     * @param model The model.
      * @param year The year.
      * @param month The month number (1 for January, 12 for December).
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}/{month:\\d{1,2}}")
-    public String listMonth(Model model, @PathVariable("year") int year, @PathVariable("month") int month) {
-        return listMonthPage(model, year, month, 1);
+    public ModelAndView listMonth(@PathVariable("year") int year, @PathVariable("month") int month) {
+        return listMonthPage(year, month, 1);
     }
 
     /**
      * Display a page of content items published in a month, ordered by the latest published first.
      *
-     * @param model The model.
      * @param year The year.
      * @param month The month number (1 for January, 12 for December).
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}/{month:\\d+}/page/{page:\\d+}")
-    public String listMonthPage(Model model, @PathVariable("year") int year, @PathVariable("month") int month,
+    public ModelAndView listMonthPage(@PathVariable("year") int year, @PathVariable("month") int month,
                                 @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.list(year, month, page));
-
-        return "contents";
+        return new ModelAndView("contents", contentService.list(year, month, page));
     }
 
     /**
      * Display the first page of content items published in a month, ordered by the latest published first.
      *
-     * @param model The model.
      * @param year The year.
      * @param month The month number (1 for January, 12 for December).
      * @param day The day number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}/{month:\\d+}/{day:\\d+}")
-    public String listDay(Model model, @PathVariable("year") int year, @PathVariable("month") int month,
+    public ModelAndView listDay(@PathVariable("year") int year, @PathVariable("month") int month,
                           @PathVariable("day") int day) {
-        return listDayPage(model, year, month, day, 1);
+        return listDayPage(year, month, day, 1);
     }
 
     /**
      * Display a page of content items published in a month, ordered by the latest published first.
      *
-     * @param model The model.
      * @param year The year.
      * @param month The month number (1 for January, 12 for December).
      * @param day The day number.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/{year:\\d+}/{month:\\d+}/{day:\\d+}/page/{page:\\d+}")
-    public String listDayPage(Model model, @PathVariable("year") int year, @PathVariable("month") int month,
+    public ModelAndView listDayPage(@PathVariable("year") int year, @PathVariable("month") int month,
                               @PathVariable("day") int day, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.list(year, month, day, page));
-
-        return "contents";
+        return new ModelAndView("contents", contentService.list(year, month, day, page));
     }
 
     /**
      * Display the first page of content items that use the provided tag. The tag is determined by its unique
      * identifier.
      *
-     * @param model The model.
      * @param id The tag identifier.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/tag/{id:\\d+}")
-    public String listByTagId(Model model, @PathVariable("id") int id) {
-        return listByTagIdPage(model, id, 1);
+    public ModelAndView listByTagId(@PathVariable("id") int id) {
+        return listByTagIdPage(id, 1);
     }
 
     /**
      * Display a page of content items that use the provided tag.
      *
-     * @param model The model.
      * @param id The tag identifier.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/tag/{id:\\d+}/page/{page:\\d+}")
-    public String listByTagIdPage(Model model, @PathVariable("id") int id, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.listByTagId(id, page));
-
-        return "contents";
+    public ModelAndView listByTagIdPage(@PathVariable("id") int id, @PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.listByTagId(id, page));
     }
 
     /**
      * Display the first page of content items that use the provided tag. The tag is determined by its slug.
      *
-     * @param model The model.
      * @param slug The tag slug.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/tag/{slug:\\D\\S+}")
-    public String listByTagSlug(Model model, @PathVariable("slug") String slug) {
-        return listByTagSlugPage(model, slug, 1);
+    public ModelAndView listByTagSlug(@PathVariable("slug") String slug) {
+        return listByTagSlugPage(slug, 1);
     }
 
     /**
      * Display a page of content items that use the provided tag.
      *
-     * @param model The model.
      * @param slug The tag slug.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/tag/{slug:\\D\\S+}/page/{page:\\d+}")
-    public String listByTagSlugPage(Model model, @PathVariable("slug") String slug, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.listByTagSlug(slug, page));
-
-        return "contents";
+    public ModelAndView listByTagSlugPage(@PathVariable("slug") String slug, @PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.listByTagSlug(slug, page));
     }
 
     /**
      * Display the first page of content items that are categorized under the specified category.
      *
-     * @param model The model.
      * @param id The category identifier.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/category/{id:\\d+}")
-    public String listByCategoryId(Model model, @PathVariable("id") int id) {
-        return listByCategoryIdPage(model, id, 1);
+    public ModelAndView listByCategoryId(@PathVariable("id") int id) {
+        return listByCategoryIdPage(id, 1);
     }
 
     /**
      * Display a page of content items that are categorized under the specified category.
      *
-     * @param model The model.
      * @param id The category identifier.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/category/{id:\\d+}/page/{page:\\d+}")
-    public String listByCategoryIdPage(Model model, @PathVariable("id") int id, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.listByCategoryId(id, page));
-
-        return "contents";
+    public ModelAndView listByCategoryIdPage(@PathVariable("id") int id, @PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.listByCategoryId(id, page));
     }
 
     /**
      * Display the first page of content items that use the provided tag. The tag is determined by its slug.
      *
-     * @param model The model.
      * @param slug The tag slug.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/category/{slug:\\D\\S+}")
-    public String listByCategorySlug(Model model, @PathVariable("slug") String slug) {
-        return listByCategorySlugPage(model, slug, 1);
+    public ModelAndView listByCategorySlug(@PathVariable("slug") String slug) {
+        return listByCategorySlugPage(slug, 1);
     }
 
     /**
      * Display a page of content items that use the provided tag.
      *
-     * @param model The model.
      * @param slug The tag slug.
      * @param page The page number.
-     * @return The template name.
+     * @return The model and view.
      */
     @GetMapping(value = "/category/{slug:\\D\\S+}/page/{page:\\d+}")
-    public String listByCategorySlugPage(Model model, @PathVariable("slug") String slug, @PathVariable("page") int page) {
-        model.addAllAttributes(contentService.listByCategorySlug(slug, page));
-
-        return "contents";
+    public ModelAndView listByCategorySlugPage(@PathVariable("slug") String slug, @PathVariable("page") int page) {
+        return new ModelAndView("contents", contentService.listByCategorySlug(slug, page));
     }
 }
