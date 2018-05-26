0b3f256e9b6743cfc3fa7a72fceb78b86b4670a9
Kostas Kokkoros <mavroprovato@gmail.com>
2018-05-26 03:28:03 +0300

    Internationalization

--- src/main/java/net/mavroprovato/springcms/SpringCmsApplication.java
+++ src/main/java/net/mavroprovato/springcms/SpringCmsApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+import java.util.Locale;
 
 @EntityScan(
     basePackageClasses = {SpringCmsApplication.class, Jsr310JpaConverters.class}
@@ -20,5 +24,12 @@ public class SpringCmsApplication {
     @Bean
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
     }
 }
