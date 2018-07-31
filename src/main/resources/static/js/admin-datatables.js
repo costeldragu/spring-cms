0b0de8d8f24f1f413eac059bc64fe1fd9a2ffa49
Kostas Kokkoros <mavroprovato@gmail.com>
2018-08-01 02:42:06 +0300

    Improve javascript for admin

--- src/main/resources/static/js/admin-datatables.js
+++ src/main/resources/static/js/admin-datatables.js
@@ -1,70 +1,44 @@
-$(document).ready(function() {
-    $('#dataTable').DataTable({
+function toSpringData(data) {
+    let d = {
+        draw: data.draw,
+        start: data.start,
+        length: data.length,
+        'search.value': data.search.value,
+        'search.regex': data.search.regex
+    };
+
+    for (let i = 0; i < data.order.length; i++) {
+        d['order[' + i + '].column'] = data.order[i].column;
+        d['order[' + i + '].dir'] = data.order[i].dir;
+    }
+
+    for (let i = 0; i < data.columns.length; i++) {
+        d['columns[' + i + '].data'] = data.columns[i].data;
+        d['columns[' + i + '].name'] = data.columns[i].name;
+        d['columns[' + i + '].searchable'] = data.columns[i].searchable;
+        d['columns[' + i + '].orderable'] = data.columns[i].orderable;
+    }
+
+    return d;
+}
+
+function renderList(data, mapFunc) {
+    return data.map(mapFunc).join(', ');
+}
+
+function renderDate(date) {
+    return date.slice(0, date.indexOf('T'));
+}
+
+function createDataTable(id, url, columns) {
+    $(`#${id}`).DataTable({
         processing: true,
         serverSide: true,
+        searching: false,
         ajax: {
-            url: '/admin/post/list',
-            data: function (data) {
-                var d = {
-                    draw: data.draw,
-                    start: data.start,
-                    length: data.length,
-                    'search.value': data.search.value,
-                    'search.regex': data.search.regex
-                };
-
-                for (var i = 0; i < data.order.length; i++) {
-                    d['order[' + i + '].column'] = data.order[i].column;
-                    d['order[' + i + '].dir'] = data.order[i].dir;
-                }
-
-                for (var i = 0; i < data.columns.length; i++) {
-                    d['columns[' + i + '].data'] = data.columns[i].data;
-                    d['columns[' + i + '].name'] = data.columns[i].name;
-                    d['columns[' + i + '].searchable'] = data.columns[i].searchable;
-                    d['columns[' + i + '].orderable'] = data.columns[i].orderable;
-                }
-
-                return d;
-            }
+            url: url,
+            data: toSpringData
         },
-        columns: [
-            {
-                data: "title",
-                orderable: true
-            }, {
-                data: "author.userName",
-                orderable: false
-            }, {
-                data: "categories",
-                orderable: false,
-                render: function(data) {
-                    var str = '';
-                    for (var i = 0; i < data.length; i++) {
-                        str += data[i].name;
-                        if (i !== data.length - 1) {
-                            str += ', '
-                        }
-                    }
-                    return str;
-                }
-            }, {
-                data: "tags",
-                orderable: false,
-                render: function(data) {
-                    var str = '';
-                    for (var i = 0; i < data.length; i++) {
-                        str += data[i].name;
-                        if (i !== data.length - 1) {
-                            str += ', '
-                        }
-                    }
-                    return str;
-                }
-            }, {
-                data: "publishedAt",
-                orderable: true
-            }
-        ]
+        columns: columns
     });
-});
+}
